4c10961b1ada1ace5c541b00a70816fb43f9e77f
Vova Traihel <vovatraigel@gmail.com>
2022-06-19 01:18:49 +0300

    05 task

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,34 +1,30 @@
-import React, {useState} from 'react'
+import React from 'react'
 import styles from './HW5.module.css'
 import {NavLink} from "react-router-dom";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Header() {
 
-    const [taskItem, setTaskItem] = useState<boolean>(false)
-    const onClickHandler = () => setTaskItem(!taskItem)
-
     return (
-        <div className={styles.header}>
-            {taskItem ? <div className={styles.navLinkItems}>
-                <NavLink
-                    className={({isActive}) => isActive ? `${styles.activeLink}` : `${styles.link}`}
-                    to={'/pre-junior'}
-                >pre-junior</NavLink>
+        <div className={styles.navLinkItems}>react homeworks:
+            <div className={styles.slide}>
+                <div className={styles.toggle}>Menu</div>
+                <div className={styles.box}>
+                    <NavLink
+                        className={({isActive}) => isActive ? `${styles.activeLink}` : `${styles.link}`}
+                        to={'/pre-junior'}
+                    >pre-junior</NavLink>
 
-                <NavLink
-                    className={({isActive}) => isActive ? `${styles.activeLink}` : `${styles.link}`}
-                    to={'/junior'}
-                >junior</NavLink>
+                    <NavLink
+                        className={({isActive}) => isActive ? `${styles.activeLink}` : `${styles.link}`}
+                        to={'/junior'}
+                    >junior</NavLink>
 
-                <NavLink
-                    className={({isActive}) => isActive ? `${styles.activeLink}` : `${styles.link}`}
-                    to={'/juniorPlus'}
-                >juniorPlus</NavLink>
-            </div> : ''}
-            <SuperButton className={styles.buttonMenu} onClick={onClickHandler}>
-                {taskItem ? 'X' : 'Menu'}
-            </SuperButton>
+                    <NavLink
+                        className={({isActive}) => isActive ? `${styles.activeLink}` : `${styles.link}`}
+                        to={'/juniorPlus'}
+                    >juniorPlus</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
