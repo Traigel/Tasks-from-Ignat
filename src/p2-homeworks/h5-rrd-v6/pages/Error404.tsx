97d3fe9c72eb6ede9643f6c52a4574f7aa30e464
Vova Traihel <vovatraigel@gmail.com>
2022-06-25 14:22:53 +0300

    05 error

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -4,9 +4,7 @@ import styles from '../HW5.module.css'
 function Error404() {
     return (
         <div className={styles.error404}>
-            <h1>404</h1>
-            <h2>Page not found!</h2>
-            <h3>—ฅ/ᐠ.̫ .ᐟ\ฅ—</h3>
+            <img alt={'qwe'} src={'https://i1.wp.com/media.giphy.com/media/8L0Pky6C83SzkzU55a/source.gif?w=525&ssl=1'}/>
         </div>
     )
 }
